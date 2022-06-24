67d4e092c81dd2effa9fdab979cbc52d25d307fa
EmanueleEsposito <manu2022@blu.it>
2022-06-24 17:16:24 +0200

    Aggiunti:attributi,costruttore,metodi e creazione nuovo utente con validazione

--- src/jana60/registrazioneUtente/Registrazione.java
+++ src/jana60/registrazioneUtente/Registrazione.java
@@ -1,10 +1,30 @@
 package jana60.registrazioneUtente;
 
+import java.util.Scanner;
+
 public class Registrazione {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		try {
+			System.out.println("Inserisci il tuo nome: ");
+			String nome = scan.nextLine();
+			System.out.println("Inserisci il tuo cognome: ");
+			String cognome = scan.nextLine();
+			System.out.println("Inserisci la tua email: ");
+			String email = scan.nextLine();
+			System.out.println("Inserisci la tua password: ");
+			String password = scan.nextLine();
+			System.out.println("Inserisci la tua età: ");
+			int etaUtente = Integer.parseInt(scan.nextLine());
+			Utente nuovoUtente = new Utente(nome, cognome, email, password, etaUtente);
+			System.out.println("Registrazione completata, benvenuto!");
+		} catch (Exception exceptionUtente) {
+			System.out.println("Impossibile terminare la registrazione");
+			System.out.println(exceptionUtente.getMessage());
 
+		}
+		scan.close();
 	}
 
 }
--- src/jana60/registrazioneUtente/Utente.java
+++ src/jana60/registrazioneUtente/Utente.java
@@ -1,5 +1,79 @@
 package jana60.registrazioneUtente;
 
 public class Utente {
+	// Attributi
+	private String nome;
+	private String cognome;
+	private String email;
+	private String password;
+	private int eta;
+
+	// Costruttore
+	public Utente(String nome, String cognome, String email, String password, int eta) throws Exception {
+		// Se email contiene un carattere @ e un . in input continuo, altrimenti sollevo
+		// un' Eccezione
+
+		if (email.matches("[a-z].[a-z]+@+[a-z].[a-z]")) {
+			throw new Exception("Email inserita non valida");
+		}
+
+		// Se password è lunga tra gli 8 e 12 caratteri in input continuo, altrimenti
+		// sollevo un'Eccezione
+		if (password.length() < 8 || password.length() > 12) {
+			throw new Exception("Password inserita non valida");
+		}
+
+		// Se eta è maggiore di 18 in input continuo altrimenti sollevo un'Eccezione
+		if (eta < 18) {
+			throw new Exception("Età dell'untente non valida");
+		}
+		// Assegno nome, cognome, email ed eta
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+		this.password = password;
+		this.eta = eta;
+	}
+
+	// Metodi getter e setter
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getEta() throws Exception {
+		return eta;
+	}
+
+	public void setEta(int eta) throws Exception {
+		this.eta = eta;
+	}
 
 }
